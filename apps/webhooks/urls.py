e7f53b5a8ef3f83beeee8622d6950da58df9986e
mdsazzadhossank <150121793+mdsazzadhossank@users.noreply.github.com>
2026-09-12 07:14:49 +0600

    Allow optional trailing slash on webhook endpoints

--- apps/webhooks/urls.py
+++ apps/webhooks/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
+from django.urls import re_path
 from .views import MessengerWebhookView, WhatsappWebhookView
 
 urlpatterns = [
-    path('messenger/', MessengerWebhookView.as_view(), name='messenger_webhook'),
-    path('whatsapp/', WhatsappWebhookView.as_view(), name='whatsapp_webhook'),
+    re_path(r'^messenger/?$', MessengerWebhookView.as_view(), name='messenger_webhook'),
+    re_path(r'^whatsapp/?$', WhatsappWebhookView.as_view(), name='whatsapp_webhook'),
 ]
